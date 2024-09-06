0b271e1d6e5676be04bf1d150030b0a3716ae937
bielgirao <bielgirao@hotmail.com>
2024-09-05 22:25:46 -0300

    feat: add coffee's data

---
+++ src/Coffees.ts
@@ -0,0 +1,116 @@
+import {CoffeeType} from "./@types/coffee";
+
+export const coffeeData: CoffeeType[] = [
+    {
+        id: 1,
+        imagePath: 'src/assets/coffees/traditional-espresso.png',
+        categories: ['Traditional'],
+        name: 'Traditional Espresso',
+        description: 'Traditional coffee made with hot water and ground beans',
+        price: 9.90
+    },
+    {
+        id: 2,
+        imagePath: 'src/assets/coffees/american-espresso.png',
+        categories: ['Traditional'],
+        name: 'American Espresso',
+        description: 'Diluted espresso, less intense than the traditional one',
+        price: 9.90
+    },
+    {
+        id: 3,
+        imagePath: 'src/assets/coffees/creamy-espresso.png',
+        categories: ['Traditional'],
+        name: 'Creamy Espresso',
+        description: 'Traditional espresso with creamy foam',
+        price: 9.90
+    },
+    {
+        id: 4,
+        imagePath: 'src/assets/coffees/iced-espresso.png',
+        categories: ['Traditional', 'Iced'],
+        name: 'Iced Espresso',
+        description: 'Drink prepared with espresso coffee and ice cubes',
+        price: 9.90
+    },
+    {
+        id: 5,
+        imagePath: 'src/assets/coffees/coffee-and-milk.png',
+        categories: ['Traditional', 'With Milk'],
+        name: 'Coffee & Milk',
+        description: 'Mixed of traditional espresso with steamed milk',
+        price: 9.90
+    },
+    {
+        id: 6,
+        imagePath: 'src/assets/coffees/latte.png',
+        categories: ['Traditional', 'With Milk'],
+        name: 'Latte',
+        description: 'A shot of espresso with double the milk and creamy foam',
+        price: 9.90
+    },
+    {
+        id: 7,
+        imagePath: 'src/assets/coffees/cappuccino.png',
+        categories: ['Traditional', 'With Milk'],
+        name: 'Cappuccino',
+        description: 'Cinnamon drink made from equal parts coffee, milk and foam',
+        price: 9.90
+    },
+    {
+        id: 8,
+        imagePath: 'src/assets/coffees/macchiato.png',
+        categories: ['Traditional', 'With Milk'],
+        name: 'Macchiato',
+        description: 'Espresso mixed with a little hot milk and foam',
+        price: 9.90
+    },
+    {
+        id: 9,
+        imagePath: 'src/assets/coffees/mochaccino.png',
+        categories: ['Traditional', 'With Milk'],
+        name: 'Mochaccino',
+        description: 'Espresso with chocolate syrup, a little milk and foam',
+        price: 9.90
+    },
+    {
+        id: 10,
+        imagePath: 'src/assets/coffees/hot-chocolate.png',
+        categories: ['Special', 'With Milk'],
+        name: 'Hot chocolate',
+        description: 'Drink made with chocolate dissolved in hot milk and coffee',
+        price: 9.90
+    },
+    {
+        id: 11,
+        imagePath: 'src/assets/coffees/cuban.png',
+        categories: ['Special', 'Alcoholic', 'Iced'],
+        name: 'Cuban',
+        description: 'Iced espresso drink with rum, cream and mint',
+        price: 9.90
+    },
+    {
+        id: 12,
+        imagePath: 'src/assets/coffees/hawaiian.png',
+        categories: ['Special'],
+        name: 'Hawaiian',
+        description: 'Sweet drink made with coffee and coconut milk',
+        price: 9.90
+    },
+    {
+        id: 13,
+        imagePath: 'src/assets/coffees/arabic.png',
+        categories: ['Special'],
+        name: 'Arabic',
+        description: 'Drink prepared with Arabic coffee beans and spices',
+        price: 9.90
+    },
+    {
+        id: 14,
+        imagePath: 'src/assets/coffees/irish.png',
+        categories: ['Special', 'Alcoholic'],
+        name: 'Irish',
+        description: 'Drink made with coffee, Irish whiskey, sugar and whipped cream',
+        price: 9.90
+    },
+];
